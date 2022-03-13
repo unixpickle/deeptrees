a4ac55bb086a3a7e2c5cb13d034391af0a34ff43
Alex Nichol <unixpickle@gmail.com>
2022-03-13 14:12:34 -0700

    branch abstraction

--- deeptrees/tree.py
+++ deeptrees/tree.py
@@ -12,19 +12,40 @@ class Tree(ABC):
 
 
 @dataclass
-class ObliqueTreeBranch(Tree):
-    coef: np.ndarray
-    threshold: np.ndarray
+class TreeBranch(Tree):
     left: Tree
     right: Tree
 
     def predict(self, xs: np.ndarray) -> np.ndarray:
-        decisions = xs @ self.coef > self.threshold
+        decisions = self.decision(xs)
         sub_left = self.left.predict(xs[np.logical_not(decisions)])
         sub_right = self.right.predict(xs[decisions])
         out = np.zeros_like(sub_left, shape=(len(xs), sub_left.shape[1]))
         out[np.logical_not(decisions)] = sub_left
         out[decisions] = sub_right
+        return out
+
+    @abstractmethod
+    def decision(self, xs: np.ndarray) -> np.ndarray:
+        pass
+
+
+@dataclass
+class AxisTreeBranch(TreeBranch):
+    axis: int
+    threshold: np.ndarray
+
+    def decision(self, xs: np.ndarray) -> np.ndarray:
+        return xs[self.axis] > self.threshold
+
+
+@dataclass
+class ObliqueTreeBranch(TreeBranch):
+    coef: np.ndarray
+    threshold: np.ndarray
+
+    def decision(self, xs: np.ndarray) -> np.ndarray:
+        return xs @ self.coef > self.threshold
 
 
 @dataclass

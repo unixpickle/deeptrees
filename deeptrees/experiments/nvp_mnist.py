8cf4646e39fe40e6f4e2a56126c95a2edb7648a3
Alex Nichol <unixpickle@gmail.com>
2022-03-21 20:33:23 -0700

    save model

--- deeptrees/experiments/nvp_mnist.py
+++ deeptrees/experiments/nvp_mnist.py
@@ -1,6 +1,7 @@
 import itertools
+import os
+import pickle
 
-import numpy as np
 import torch
 import torch.optim as optim
 from deeptrees.cascade import Batch, CascadeSGD
@@ -10,8 +11,13 @@ from deeptrees.experiments.boosting_mnist import dataset_to_tensors
 from deeptrees.fit_torch import TorchObliqueBranchBuilder
 from torchvision.datasets.mnist import MNIST
 
+OUTPUT_DIR = "./models_nvp_mnist"
+SAVE_INTERVAL = 10
+
 
 def main():
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+
     print("loading data...")
     train_dataset = MNIST("./mnist_data", train=True, download=True)
     test_dataset = MNIST("./mnist_data", train=False, download=True)
@@ -32,6 +38,7 @@ def main():
     sgd_model = CascadeSGD(
         model, interval=5, opt=optim.Adam(model.parameters(), lr=1e-3)
     )
+    print(f"model has {sum(x.numel() for x in model.parameters())} parameters.")
 
     print("training...")
     for epoch in itertools.count():
@@ -46,6 +53,9 @@ def main():
         print(
             f"epoch {epoch}: train_loss={losses.mean().item():.05} test_loss={test_losses.mean().item():.05}"
         )
+        if (epoch + 1) % SAVE_INTERVAL == 0:
+            with open(os.path.join(OUTPUT_DIR, "model.pkl"), "wb") as f:
+                pickle.dump(model, f)
 
 
 if __name__ == "__main__":
